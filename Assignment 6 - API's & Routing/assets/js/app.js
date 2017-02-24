00eac48db3f4fa0ccdad47484ab6446d1bc8f048
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-02-24 16:05:06 +0100

    Change: Code refactoring & saving data in localStorage

--- Assignment 6 - API's & Routing/assets/js/app.js
+++ Assignment 6 - API's & Routing/assets/js/app.js
@@ -1,5 +1,4 @@
 /* Sources:
-  - http://handlebarsjs.com/
   - http://projects.jga.me/routie/
   - http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
   - http://stackoverflow.com/questions/413439/how-to-dynamically-change-a-web-pages-title
@@ -7,46 +6,90 @@
   - https://developers.themoviedb.org/3
 */
 
+/* TODO:
+    - Async api calls for caching data
+    - Search on keywords/genres
+    - Search for movies.
+    - Placeholder image for movies without img.
+    - Random movie -> catch errors and show other movie
+    - Reload random movie
+*/
+
 (() => {
   "use strict"
+
+  /* Saving sections to variables
+  --------------------------------------------------------------*/
   const movieList = document.getElementsByClassName('movie_list')[0];
   const movieSingle = document.getElementsByClassName('movie_single')[0];
 
-  const app = {
-    init() {
-      // window.location.hash = 'trending';
-    },
-    getData(filter) {
-      const request = new XMLHttpRequest();
-      const apiKey = '?api_key=76244b12adc0042d55a0f0f57905f0be';
-      const getUrl = `https://api.themoviedb.org/3/${filter}${apiKey}`;
-
-      request.open('GET', getUrl, true);
-      request.onload = () => {
-        if (request.status >= 200 && request.status < 400) {
-          let data = JSON.parse(request.responseText);
-          cleanData.init(data);
-        } else {
-          window.location.hash = 'random';
-        }
-      };
-      request.onerror = () => {
-        console.error('Error');
-      };
-      request.send();
-    }
+  /* All standard filters for displaying movies
+  --------------------------------------------------------------*/
+  const allFilters = {
+    trending: 'movie/popular',
+    toplist: 'movie/top_rated',
+    latest: 'movie/now_playing',
+    upcoming: 'movie/upcoming'
   };
 
+  /* Initialize app - Get al standard data and save it in object
+  --------------------------------------------------------------*/
+  const app = {
+    init() {
+        getData(allFilters.trending, 'popular');
+        getData(allFilters.toplist, 'toplist');
+        getData(allFilters.latest, 'latest');
+        getData(allFilters.upcoming, 'upcoming');
+
+        this.startPage();
+    },
+    startPage() {
+      window.location.hash = "trending";
+    }
+  }
+
+  /* Function for getting data from the API
+  --------------------------------------------------------------*/
+  const getData = (filter, key) => {
+    const request = new XMLHttpRequest();
+    const apiKey = '?api_key=76244b12adc0042d55a0f0f57905f0be';
+    const getUrl = `https://api.themoviedb.org/3/${filter}${apiKey}`;
+
+    request.open('GET', getUrl, true);
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        let data = request.responseText;
+        let checkData = JSON.parse(request.responseText);
+        checkData.key = key;
+        if(!checkData.results) {
+          cleanData.init(checkData);
+        } else if(checkData.key === 'similar') {
+          cleanData.init(checkData);
+        } else {
+          localStorage.setItem(key, data);
+        }
+      } else {
+        window.location.hash = 'random';
+      }
+    };
+    request.onerror = () => {
+      console.error('Error');
+    };
+    request.send();
+  };
+
+  /* Check if the data is list or single, and clean up
+  --------------------------------------------------------------*/
   const cleanData = {
     init(originalData) {
       if (!originalData.results) {
-        this.cleanSingle(originalData);
+        this.single(originalData);
       } else {
-        this.cleanList(originalData);
+        this.list(originalData);
       }
     },
 
-    cleanList(data) {
+    list(data) {
       data.results.map(function(el) {
         el.backdrop_path = `https://image.tmdb.org/t/p/w500/${el.backdrop_path}`;
       });
@@ -68,13 +111,12 @@
       showList(data.results, attributes);
     },
 
-    cleanSingle(data) {
+    single(data) {
       data.poster_path = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
       data.budget = formatCurrency(data.budget);
       data.revenue = formatCurrency(data.revenue);
       data.runtime = `${(data.runtime / 60).toFixed(1)} uur`;
       data.imdb_id = `http://www.imdb.com/title/${data.imdb_id}`;
-      data.production_companies.name = 'hola'
       let attributes = {
         movie_image: {
           src: function() {
@@ -114,31 +156,35 @@
   routie({
     'trending': () => {
       document.title = 'Trending movies';
-      app.getData('movie/popular');
+      let trending_data = JSON.parse(localStorage.getItem('popular'));
+      cleanData.list(trending_data);
     },
     'toplist': () => {
       document.title = 'Top rated movies'
-      app.getData('movie/top_rated');
+      let toplist_data = JSON.parse(localStorage.getItem('toplist'));
+      cleanData.list(toplist_data);
     },
     'latest': () => {
       document.title = 'Latest movies'
-      app.getData('movie/now_playing');
+      let latest_data = JSON.parse(localStorage.getItem('latest'));
+      cleanData.list(latest_data);
     },
     'upcoming': () => {
       document.title = 'Upcoming movies'
-      app.getData('movie/upcoming');
+      let upcoming_data = JSON.parse(localStorage.getItem('upcoming'));
+      cleanData.list(upcoming_data);
     },
     'movie/:id/:title': (id, title) => {
       document.title = `Movie: ${title}`;
-      app.getData(`movie/${id}`);
+      getData(`movie/${id}`, 'single');
     },
     'random': () => {
       let random = Math.floor((Math.random() * 1000) + 100);
-      app.getData(`movie/${random}`);
+      getData(`movie/${random}`, 'random');
     },
     'movie/:id/:title/similar': (id, title) => {
       document.title = `Movies like: ${title}`;
-      app.getData(`movie/${id}/similar`);
+      getData(`movie/${id}/similar`, 'similar');
     }
   });
 
--- Assignment 6 - API's & Routing/assets/js/sandbox/app.js
+++ Assignment 6 - API's & Routing/assets/js/sandbox/app.js
@@ -1,4 +1,5 @@
 /* Sources:
+  - http://handlebarsjs.com/
   - http://projects.jga.me/routie/
   - http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
   - http://stackoverflow.com/questions/413439/how-to-dynamically-change-a-web-pages-title
@@ -6,85 +7,46 @@
   - https://developers.themoviedb.org/3
 */
 
-/* TODO:
-    - Async api calls for caching data
-    - Search on keywords/genres
-    - Search for movies.
-    - Placeholder image for movies without img.
-    - Random movie -> catch errors and show other movie
-    - Reload random movie
-*/
-
 (() => {
   "use strict"
-
-  /* Saving sections to variables
-  --------------------------------------------------------------*/
   const movieList = document.getElementsByClassName('movie_list')[0];
   const movieSingle = document.getElementsByClassName('movie_single')[0];
 
-  /* All standard filters for displaying movies
-  --------------------------------------------------------------*/
-  const allFilters = {
-    trending: 'movie/popular',
-    toplist: 'movie/top_rated',
-    latest: 'movie/now_playing',
-    upcoming: 'movie/upcoming'
-  };
-
-  const allData = {};
-
-  /* Initialize app - Get al standard data and save it in object
-  --------------------------------------------------------------*/
   const app = {
     init() {
-        getData(allFilters.trending, 'popular');
-        getData(allFilters.toplist, 'toplist');
-        getData(allFilters.latest, 'latest');
-        getData(allFilters.upcoming, 'upcoming');
-
-        this.startPage();
+      // window.location.hash = 'trending';
     },
-    startPage() {
-      window.location.hash = "trending";
+    getData(filter) {
+      const request = new XMLHttpRequest();
+      const apiKey = '?api_key=76244b12adc0042d55a0f0f57905f0be';
+      const getUrl = `https://api.themoviedb.org/3/${filter}${apiKey}`;
+
+      request.open('GET', getUrl, true);
+      request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+          let data = JSON.parse(request.responseText);
+          cleanData.init(data);
+        } else {
+          window.location.hash = 'random';
+        }
+      };
+      request.onerror = () => {
+        console.error('Error');
+      };
+      request.send();
     }
-  }
-
-  /* Function for getting data from the API
-  --------------------------------------------------------------*/
-  const getData = (filter, key) => {
-    const request = new XMLHttpRequest();
-    const apiKey = '?api_key=76244b12adc0042d55a0f0f57905f0be';
-    const getUrl = `https://api.themoviedb.org/3/${filter}${apiKey}`;
-
-    request.open('GET', getUrl, true);
-    request.onload = () => {
-      if (request.status >= 200 && request.status < 400) {
-        let data = JSON.parse(request.responseText);
-        data.filter = key;
-        cleanData.init(data);
-      } else {
-        window.location.hash = 'random';
-      }
-    };
-    request.onerror = () => {
-      console.error('Error');
-    };
-    request.send();
   };
 
-  /* Check if the data is list or single, and clean up
-  --------------------------------------------------------------*/
   const cleanData = {
     init(originalData) {
       if (!originalData.results) {
-        this.single(originalData);
+        this.cleanSingle(originalData);
       } else {
-        allData[originalData.filter] = originalData;
+        this.cleanList(originalData);
       }
     },
 
-    list(data) {
+    cleanList(data) {
       data.results.map(function(el) {
         el.backdrop_path = `https://image.tmdb.org/t/p/w500/${el.backdrop_path}`;
       });
@@ -106,12 +68,13 @@
       showList(data.results, attributes);
     },
 
-    single(data) {
+    cleanSingle(data) {
       data.poster_path = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
       data.budget = formatCurrency(data.budget);
       data.revenue = formatCurrency(data.revenue);
       data.runtime = `${(data.runtime / 60).toFixed(1)} uur`;
       data.imdb_id = `http://www.imdb.com/title/${data.imdb_id}`;
+      data.production_companies.name = 'hola'
       let attributes = {
         movie_image: {
           src: function() {
@@ -151,7 +114,7 @@
   routie({
     'trending': () => {
       document.title = 'Trending movies';
-      console.log(allData);
+      app.getData('movie/popular');
     },
     'toplist': () => {
       document.title = 'Top rated movies'
@@ -187,8 +150,5 @@
   };
 
   app.init();
-
-  console.log(Object.keys(allData));
-  console.log(JSON.stringify(allData));
 
 })();
